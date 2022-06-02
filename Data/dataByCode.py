6d518636ce009b4887fa3edd23ac69e5d82dd8cd
Youngseo Yoo <7dudtj@naver.com>
2022-06-02 10:30:01 +0900

    dataByCode 모듈 추가

--- Data/dataByCode.py
+++ Data/dataByCode.py
@@ -6,15 +6,16 @@
 # Date, Close, ma5, ma20, ma60
 # ma5_Change, ma20_Change, ma60_Change
 #
-# 사용 예시 : getData('20200106', '20220429', '000060')
+# 사용 예시 : getData('000060', '20200106', '20220429')
 
 # import libraries
 import FinanceDataReader as fdr
 import numpy as np
 from datetime import datetime, timedelta
+import math
 
 # 주어진 기간에 해당하는 데이터프레임 반환 함수
-def getData(start_date, end_date, code):
+def getData(code, start_date, end_date):
 
     # 시작일이 주말인 경우: 예외처리
     start_datetime = datetime.strptime(start_date, '%Y%m%d')
@@ -25,12 +26,8 @@ def getData(start_date, end_date, code):
         start_datetime += timedelta(days=1)
         start_date = start_datetime.strftime('%Y%m%d')
 
-    # start_data를 조정하여 ma 계산에 필요한 여분 데이터 확보
-    new_start_datetime = start_datetime - timedelta(weeks=14)
-    new_start_date = new_start_datetime.strftime('%Y%m%d')
-
     # 종목코드에 대한 종가 데이터 수집
-    data = fdr.DataReader(code, new_start_date, end_date).reset_index()
+    data = fdr.DataReader(code, start_date, end_date)
     data.drop(['Open', 'High', 'Low', 'Change', 'Volume'], axis=1, inplace=True)
 
     # 5/20/60일 이동평균 계산
@@ -41,20 +38,21 @@ def getData(start_date, end_date, code):
     ma60 = data['Close'].rolling(window=60).mean()
     data['ma60'] = ma60
 
+    # NaN 데이터 제거: ma5는 Close로, ma20은 ma5로, ma60은 ma20으로 대체
+    for idx in data.index:
+        if (math.isnan(data.loc[idx, 'ma5'])):
+            data.loc[idx, 'ma5'] = data.loc[idx, 'ma20'] = data.loc[idx, 'ma60'] = data.loc[idx, 'Close']
+        if (math.isnan(data.loc[idx, 'ma20'])):
+            data.loc[idx, 'ma20'] = data.loc[idx, 'ma60'] = data.loc[idx, 'ma5']
+        if (math.isnan(data.loc[idx, 'ma60'])):
+            data.loc[idx, 'ma60'] = data.loc[idx, 'ma20']
+
     # 이동평균 변화율(Change) 계산
     data['ma5_Change'] = np.ceil((data['ma5']/data['ma5'].shift(1) - 1) * 100 * 10000)/10000
     data['ma20_Change'] = np.ceil((data['ma20']/data['ma20'].shift(1) - 1) * 100 * 10000)/10000
     data['ma60_Change'] = np.ceil((data['ma60']/data['ma60'].shift(1) - 1) * 100 * 10000)/10000
-
-    # Nan 데이터 제거
-    for idx in data.index:
-        if (data.loc[idx, 'Date'].to_pydatetime().strftime('%Y%m%d') != start_date):
-            data.drop(idx, inplace=True)
-        else:
-            break
-    data.reset_index(inplace=True)
-    data.drop(['index'], axis=1, inplace=True)
-
+    data.fillna(method='bfill', inplace=True)
+            
     # 이동평균 데이터타입 변환 (float to int)
     data['ma5'] = data['ma5'].astype(int)
     data['ma20'] = data['ma20'].astype(int)
